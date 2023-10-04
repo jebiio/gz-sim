8ec5047c063ea969b73b5d0b68b53a1d779c1fc1
yslee <ptss37@gmail.com>
2023-10-04 18:13:32 +0900

    add opticalflow system plugin

---
+++ src/systems/optical_flow/OpticalFlow.hh
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef OPTICALFLOW_HH_
+#define OPTICALFLOW_HH_
+
+#include <memory>
+#include <gz/sim/config.hh>
+#include <gz/sim/System.hh>
+#include <gz/sensors/Sensor.hh>
+
+namespace custom
+  // Forward declarations.
+  class OpticalFlowPrivate;
+
+  /** \class OpticalFlow OpticalFlow.hh \
+   *  gz/sim/systems/OpticalFlow.hh
+  **/
+  class OpticalFlow:
+    public System,
+    public ISystemPreUpdate,
+    public ISystemPostUpdate
+  {
+    /// \brief Constructor
+    public: explicit OpticalFlow();
+
+    /// \brief Destructor
+    public: ~OpticalFlow() override;
+
+    /// Documentation inherited
+    public: void PreUpdate(const UpdateInfo &_info,
+                           EntityComponentManager &_ecm) final;
+
+
+    /// Documentation inherited
+    public: void PostUpdate(const UpdateInfo &_info,
+                            const EntityComponentManager &_ecm) final;
+
+    /// \brief Private data pointer.
+    private: std::unique_ptr<OpticalFlowPrivate> dataPtr;
+  };
+}
+#endif
